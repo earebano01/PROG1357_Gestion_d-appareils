bac51415759eb990a65737142a3d45fcd5187849
Adonis Rebano <earebano01@monccnb.ca>
2024-02-05 00:24:01 -0400

    nettoye le code

--- src/com/exercice4/Actuateur.java
+++ src/com/exercice4/Actuateur.java
@@ -1,24 +1,3 @@
-// package com.exercice4;
-
-// import java.sql.Timestamp;
-
-// public class Actuateur extends ObjetConnecte {
-//     public String typeAction;
-//     public Double temperature;
-//     public Double humidity;
-
-//     public Actuateur(String nom, String deviceID, String type, String typeAction, Double temperature, Double humidity, Timestamp date, String status) {
-//         super(nom, deviceID, type, null, temperature, humidity, typeAction, date, status); 
-//         this.typeAction = typeAction;
-//         this.temperature = temperature;
-//         this.humidity = humidity;
-//     }
-
-//     public void actionner() {
-//         System.out.println("\nActionneur: " + nom + "\nEffectue une action de type: " + typeAction);
-//     }
-// }
-
 package com.exercice4;
 
 import java.sql.Timestamp;
--- src/com/exercice4/Capteur.java
+++ src/com/exercice4/Capteur.java
@@ -1,25 +1,3 @@
-// package com.exercice4;
-
-// import java.sql.Timestamp;
-
-// public class Capteur extends ObjetConnecte {
-//     public String typeMesure;
-//     public Double temperature;
-//     public Double humidity;
-
-//     public Capteur(String nom, String deviceID, String type, String typeMesure, Double temperature, Double humidity, Timestamp date, String status) {
-//         super(nom, deviceID, type, typeMesure, temperature, humidity, null, date, status);  
-//         this.typeMesure = typeMesure;
-//         this.temperature = temperature;
-//         this.humidity = humidity;
-//     }
-
-//     public void mesurer(Simulator sim) {
-//         System.out.println("\nCapteur: " + nom + "\nType de Mesure: " + typeMesure);
-//     }
-    
-// }
-
 package com.exercice4;
 
 import java.sql.Timestamp;
