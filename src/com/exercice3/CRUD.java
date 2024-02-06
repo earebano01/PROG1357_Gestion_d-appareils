4049692bf0206e55ba0156a9a13340941403a6c0
Adonis Rebano <earebano01@monccnb.ca>
2024-02-06 01:35:23 -0400

    modifié le CRUD et les tables du BD

--- src/com/exercice3/CRUD.java
+++ src/com/exercice3/CRUD.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
 public class CRUD {
 
     public static int insertObjetConnecte(String nom, String device_id, String type, String typemesure, String typeaction) {
